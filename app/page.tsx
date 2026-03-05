19a535459ce9b0e32d64a445b25442254af6164e
yahyasahaja <yahya.sahaja2@gmail.com>
2026-03-05 08:24:30 +0700

    remove unused information

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import Link from "next/link";
-
 export default function HomePage() {
   return (
     <main style={{ padding: "2rem", maxWidth: "600px", margin: "0 auto" }}>
       <h1>LapakGaming – Interview</h1>
-      <p>
-        <strong>Task:</strong> Build the Product Detail Page (PDP).
-      </p>
-      <p>
-        <Link href="/products/game-123">→ Go to PDP (product: game-123)</Link>
-      </p>
     </main>
   );
 }
